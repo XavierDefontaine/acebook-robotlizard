b8a2f5c9b38d24aec4e5b0f53b8cf1db06dc1bae
olliesmith3 <olliesmith3@rocketmail.com>
2020-10-28 15:41:18 +0000

    Added profile visiting test

--- spec/features/user_can_edit_comment_spec.rb
+++ spec/features/user_can_edit_comment_spec.rb
@@ -8,4 +8,10 @@ RSpec.feature "Timeline", type: :feature do
     sign_in
     add_post
     add_comment
+    find("input[type=submit][value='Edit Comment']").click
+    fill_in :comment_content, with: "I walk alone"
+    find("input[type=submit][value='Submit']").click
+    expect(page).to have_content("I walk alone")
+  end
+end
 
