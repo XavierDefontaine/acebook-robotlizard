3cb2746d85983d6194b04a104c6344aecf9f8763
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-10-28 12:29:34 +0000

    added sign up helper

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require '/Users/linusmjornstedt/Projects/acebook-robotlizard/spec/helpers/features_helper.rb'
 
 RSpec.feature "Timeline", type: :feature do
 
@@ -11,28 +12,22 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   scenario "Can submit posts and view them" do
-    #sign_up
-    visit "/users/sign_up"
-    fill_in :user_first_name, with: "linus"
-    fill_in :user_last_name, with: "smit"
-    fill_in :user_last_name, with: "smit"
-    fill_in :user_email, with: "linus@ollie.com"
-    fill_in :user_password, with: "password123"
-    fill_in :user_password_confirmation, with: "password123"
-    click_button "Sign Up"
-    User.find(first_name: "linus").skip_confirmation!
+    sign_up
 
-    #log_in
-    fill_in :email, with: "linus@ollie.com"
-    fill_in :password, with: "password123"
+    expect(page).to have_content("Forgot your password?")
+    # User.find(1).skip_confirmation!
+
+    # #log_in
+    # fill_in :email, with: "linus@ollie.com"
+    # fill_in :password, with: "password123"
 
 
-    expect(page).to have_content("What's on your mind?")
-
-
-    # fill_in :message, with: "Hello, world!"
-    # click_button "Submit"
     # expect(page).to have_content("What's on your mind?")
-    # expect(page).to have_content("01/01/1994 0:00")
+
+
+    # # fill_in :message, with: "Hello, world!"
+    # # click_button "Submit"
+    # # expect(page).to have_content("What's on your mind?")
+    # # expect(page).to have_content("01/01/1994 0:00")
   end
 end
--- spec/helpers/features_helper.rb
+++ spec/helpers/features_helper.rb
@@ -1,10 +1,10 @@
 def sign_up
   visit "/users/sign_up"
-  fill_in :first_name, with: "linus"
-  fill_in :last_name, with: "smit"
-  fill_in :last_name, with: "smit"
-  fill_in :email, with: "linus@ollie.com"
-  fill_in :password, with: "password123"
-  fill_in :password_confirmation, with: "password123"
-  click_button "Sign Up"
+  fill_in :user_first_name, with: "linus"
+  fill_in :user_last_name, with: "smit"
+  fill_in :user_last_name, with: "smit"
+  fill_in :user_email, with: "linus@ollie.com"
+  fill_in :user_password, with: "password123"
+  fill_in :user_password_confirmation, with: "password123"
+  find("input[type=submit][value='Sign up']").click
 end
