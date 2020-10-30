99ab2b31f79937af8c63873ba340ed303915dbf6
Xavier Defontaine <xdefontaine@gmail.com>
2020-10-30 16:15:47 +0000

    fixed post/comments deleting cascading error after user deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_friendship
   acts_as_voter
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
