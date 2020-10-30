361b257a92bfc0699ae2421f2452e345573a4d75
Xavier Defontaine <xdefontaine@gmail.com>
2020-10-30 12:06:54 +0000

    can log out test passed

--- spec/features/user_can_log_out_spec.rb
+++ spec/features/user_can_log_out_spec.rb
@@ -1,3 +1,4 @@
+require 'rails_helper'
 require_relative '../helpers/features_helper.rb'
 
 RSpec.feature "Timeline", type: :feature do
@@ -5,7 +6,7 @@ RSpec.feature "Timeline", type: :feature do
   scenario "Can log out" do
     sign_up
     sign_in
-    click_link("Logout")
+    click_link("Log out")
     expect(page).to have_content("You need to sign in or sign up before continuing.")
   end
 end
